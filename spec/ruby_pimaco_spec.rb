0551e92098d05a2c6f81803ebc45fadd9b8aa105
puppe1990 <matheus.puppe90@hotmail.com>
2022-05-13 12:52:59 -0300

    initial project

---
+++ spec/ruby_pimaco_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RubyPimaco do
+  it "has a version number" do
+    expect(RubyPimaco::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
